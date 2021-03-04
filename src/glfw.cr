f9db75879813fbecd8570619b44d0417a8552f19
lodenos <nikolaiilodenos@gmail.com>
2021-03-04 20:39:45 +0000

    update for crystal 0.36.1

--- src/glfw.cr
+++ src/glfw.cr
@@ -1,17 +1,2 @@
 require "./lib/*"
 require "./module/*"
-
-################################################################################
-# Test
-################################################################################
-
-GLFW::Context.init
-
-window = GLFW::Window.new 1280, 720
-window.show
-
-while window.should_close == 0
-  LibGLFW.pollEvents
-end
-
-GLFW::Context.terminate
